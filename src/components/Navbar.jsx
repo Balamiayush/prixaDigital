62654a4f1bda552da45e2fa1d28e6918b4615ba9
Balamiayush <aryanbalami54@gmail.com>
2025-06-11 14:03:30 +0545

    refactor: reorganize Button component and update Navbar and Product imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from './button/Button'
 
 const Navbar = () => {
   return (
@@ -6,17 +7,12 @@ const Navbar = () => {
         <div className="container w-container">
           <a href="#" className="brand w-nav-brand"><img src="images/logo-prixa.svg" loading="lazy" width="200" alt=""/></a>
           <header role="navigation" className="nav-menu w-nav-menu">
-            <a href="#" className="menu-item w-nav-link">About</a>
-            <a href="#" className="menu-item w-nav-link">Our Work</a>
-            <a href="#" className="menu-item w-nav-link">Testemonial</a>
-            <a href="#" className="button ghost w-inline-block">
-              <div className="text-block-3">View Case Studies</div><img src="images/arrow-btn.svg" loading="lazy" alt=""/>
-            </a>
-            <a href="#" className="button w-inline-block">
-              <div className="text-block-2">Contact Us</div><img src="images/arrow-dark.svg" loading="lazy" alt=""/>
-            </a>
+            <a href="#" className="menu-item w-nav-link link">About</a>
+            <a href="#" className="menu-item w-nav-link link">Our Work</a>
+            <a href="#" className="menu-item w-nav-link link">Testemonial</a>
           </header>
           <div className="w-nav-button">
+            <Button />
             <div className="icon w-icon-nav-menu"></div>
           </div>
         </div>
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.css'; // or use CSS Modules if preferred
+
+const Button = () => {
+  return (
+   
+      <h3 className="container btn text-white text-2xl">
+        <span className="upper ">Menu</span>
+        <span className="lower ">Menu</span>
+      </h3>
+    
+  );
+};
+
+export default Button;
