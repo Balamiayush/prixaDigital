6778d20c82d0fc12a8c87449be8a1696a6e0de96
Balamiayush <aryanbalami54@gmail.com>
2025-06-11 17:37:12 +0545

    fff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-        <div className="container w-container">
-          <a href="#" className="brand w-nav-brand"><img src="images/logo-prixa.svg" loading="lazy" width="200" alt=""/></a>
-          <header role="navigation" className="nav-menu w-nav-menu">
-            <a href="#" className="menu-item w-nav-link link">About</a>
-            <a href="#" className="menu-item w-nav-link link">Our Work</a>
-            <a href="#" className="menu-item w-nav-link link">Testemonial</a>
-            <a href="#" className="button ghost w-inline-block">
-              <div className="text-block-3">View Case Studies</div><img src="images/arrow-btn.svg" loading="lazy" alt=""/>
+
+       <div data-animation="over-right" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
+        <div class="container w-container">
+          <a href="#" class="brand w-nav-brand"><img src="images/logo-prixa.svg" loading="lazy" width="200" alt=""/></a>
+          <header role="navigation" class="nav-menu w-nav-menu">
+            <a href="#" class="menu-item w-nav-link">About</a>
+            <a href="#" class="menu-item w-nav-link">Our Work</a>
+            <a href="#" class="menu-item w-nav-link">Testemonial</a>
+            <a href="#" class="button ghost w-inline-block">
+              <div class="text-block-3">View Case Studies</div><img src="images/arrow-btn.svg" loading="lazy" alt=""/>
             </a>
-            <a href="#" className="button w-inline-block">
-              <div className="text-block-2">Contact Us</div><img src="images/arrow-dark.svg" loading="lazy" alt=""/>
+            <a href="#" class="button w-inline-block">
+              <div class="text-block-2">Contact Us</div><img src="images/arrow-dark.svg" loading="lazy" alt=""/>
             </a>
           </header>
-          <div className="w-nav-button">
-            <div className="icon w-icon-nav-menu"></div>
+          <div class="w-nav-button">
+            <div class="icon w-icon-nav-menu"></div>
           </div>
         </div>
-
+      </div> 
   )
 }
 
