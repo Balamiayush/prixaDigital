c1a5edca3aace7a57b6e95b379c549e31d91bfdf
Balamiayush <aryanbalami54@gmail.com>
2025-06-20 09:59:45 +0545

    Refactor Product component for improved layout consistency and responsiveness; adjust spacing and flex properties for better alignment.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,15 +3,15 @@ import Button from "./button/Button";
 
 function Product({ val, mover, count }) {
   return (
-    <div className="w-full relative lg:top-[4rem]   left-10 py-10 md:py-20 h-auto md:h-[20rem]  text-white md:mb-10">
+    <div className="w-full  max-w-screen-xl mb-10  relative flex items-center justify-center    text-white h-[20rem]">
       <div
         onMouseEnter={() => { mover(count) }}
-        className="max-w-screen-xl   flex flex-col md:flex-row items-center md:items-center justify-between overflow-hidden"
+        className="max-w-screen-xl w-full   flex  items-center  justify-between overflow-hidden"
       >
-        <h1 className="titleProduct  capitalize font-medium mb-4 md:mb-0">{val.title}</h1>
+        <h1 className="titleProduct  capitalize font-medium ">{val.title}</h1>
         <div className="dets w-full md:w-1/4">
-          <p className="mb-6 md:mb-10">{val.description}</p>
-          <div className="flex items-center gap-3 md:gap-5">
+          <p className=" mb-10">{val.description}</p>
+          <div className="flex items-center ">
             {val.live && (
               <Button
                 text="Case Study"
